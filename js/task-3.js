06cdc3c8a99a7a0ef061c3dba0561ef6a4eaf8da
Roman Fliunt <romanflyn@gmail.com>
2025-01-19 00:43:13 +0200

    4

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    return content + padding * 2 + border * 2;
+    return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
 }
 
 console.log(getElementWidth(Number.parseInt("50px"), Number.parseInt("8px"), Number.parseInt("4px"))); // 74
